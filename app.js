572a0d656cee636701a3713bcca9b036a1cc0a9b
revi <oliiv@outlook.com>
2020-09-03 10:21:30 -0300

    first stage
    * arreglando el menu
    
    * menu de la izquierda
    
    * quitando css que no funciono
    
    * tomando los estilos reales
    
    * Separacion de estilos
    
    * estilos para el primer bloque
    
    * alineacion del texto
    
    * arreglando el menu
    
    * cambio de clases
    
    * cambios para header - botonera - primer bloque
    
    * creacion de segundo bloque
    
    * bloque 2
    
    * en la segunda version del bloque minor
    
    * arreglo de posiciones
    
    * Integracion de animaciones y mobile
    
    * cambiando clases
    
    * bloque de imagenes link
    
    * desktop section 1
    
    * poniendo clases
    
    * detalles
    
    * section 2
    
    * estilos mobile
    
    * eliminando templates
    
    * actualizaciones
    
    * section 3
    
    * section 4
    
    * section 5
    
    * footer
    
    * section 2
    
    * section 3
    
    * section 4
    
    * section 5
    
    * footer
    
    * format css
    
    * links
    
    * menu
    
    * menu
    
    * animaciones
    
    * arreglos

--- app.js
+++ app.js
@@ -1,24 +1,133 @@
-// Animation Menu Flotante
+/**
+ * Ready to use
+ */
 
-var navbar = document.querySelector('.heading')
-var button = document.querySelector('.button-menu')
-var logo = document.querySelector('.logito')
+$(document).ready( function () {
+  $(window).on('load resize', function(){ 
+    resizer();
+  });
 
+  buttonMenu()
+  menuModel()
 
-var a = window.getComputedStyle(document.querySelector('.logito'), ':before');
+  scrollHandler()
+  toggleWidgetButtons()
 
-window.onscroll = function() {
+  animationImageLink('.link-image')
+  animationImageLink('.link-title')
 
-  // pageYOffset or scrollY
-  if (window.pageYOffset > 800) {
-    navbar.classList.add('scrolled')
-    button.classList.add('menucolor')
-    logo.classList.add('hola')
-    a.classList.add('menucolor1')
-  } else {
-    navbar.classList.remove('scrolled')
-    button.classList.remove('menucolor')
-    logo.classList.remove('hola')
-    a.classList.remove('menucolor1')
+})
+
+function resizer() {
+  var w = $(window).width();
+  if (w < 768) {
+      isMobile = true;
+      $('.button-widget').addClass('button-widget-invisible')
+  } else { 
+      isMobile = false;
   }
+}
+
+/**
+ * Abrir y Cerrar el menu model
+ */
+
+function menuModel () {
+  $('.buttons-widget .button-widget').mouseenter( function () {
+    $('.container-widget').addClass('open-widget');
+  })
+  
+  $('.buttons-widget .button-widget').mouseleave( function () {
+    $('.container-widget').removeClass('open-widget');
+  })
+}
+
+
+/**
+ * Animacion para la imagen y el link titulo
+ */
+
+function animationImageLink (ele) {
+  $(ele).mouseenter(animacionHoverImagenEnter)
+  $(ele).mouseleave(animacionHoverImagenLeave)
+}
+
+function animacionHoverImagenEnter () {
+  if (!!$(this).parents('.grid').length) $(this).parents('.grid').addClass('titulo-hover')
+  if (!!$(this).parents('.block-img').length) $(this).parents('.block-img').addClass('titulo-hover')
+}
+
+function animacionHoverImagenLeave () {
+  if (!!$(this).parents('.grid').length) $(this).parents('.grid').removeClass('titulo-hover')
+  if (!!$(this).parents('.block-img').length) $(this).parents('.block-img').removeClass('titulo-hover')
+}
+
+/**
+ * Button Menu Open and Close
+ */
+
+function buttonMenu () {
+  var buttonMenuOpen = $('.button-menu-open')
+  var buttonMenuClose = $('.button-menu-close')
+  var body = $('body')
+
+  buttonMenuOpen.click(menuHandler)
+  buttonMenuClose.click(menuHandler)
+
+  function menuHandler () {
+    var menuPagina = $('.menu-pagina');
+
+    if (menuPagina.hasClass('active')) {
+      body.removeClass('menu-active')
+      menuPagina.removeClass('active')
+    } else {
+      body.addClass('menu-active')
+      menuPagina.addClass('active')
+    }
+  }
+  
+}
+
+
+
+/**
+ * Scroll para el header
+ */
+
+function scrollHandler () {
+  var lastScrollTop = 0;
+  $(document).scroll( function() {
+    var st = $(this).scrollTop();
+    var pbh = $('.section-1').height()
+
+    if (st > lastScrollTop){
+      $('.container-header').addClass('scroll-down')
+    } else {
+      $('.container-header').removeClass('scroll-down')
+    }
+
+    if ( st > pbh/2 ) {
+      $('.container-header').addClass('alternative-scroll')
+    } else {
+      $('.container-header').removeClass('alternative-scroll')
+    }
+    lastScrollTop = st;
+  });
+}
+
+ 
+/**
+ * Buttons actions
+ */
+
+function toggleWidgetButtons () {
+  $('.buttons-widget .button-mobile').click( function () {
+    var cw = $('.container-widget');
+ 
+    if ( cw.hasClass('open-widget') ) {
+     cw.removeClass('open-widget')
+    } else {
+     cw.addClass('open-widget')
+    }
+  })
 }
